c5d27ba42cbeda58db7886dd6585ef02424ef808
Yogu <info@yogularm.de>
2012-10-08 15:40:27 +0200

    Fixed input to work in Chrome
    In contrast to Gecko, Webkit does not provide the KeyEvent class which
    was used to obtain the VK_* contstants. To get it work in chrome, the
    constants now are included in the input.js script.

--- scripts/input.js
+++ scripts/input.js
@@ -2,6 +2,13 @@ self.Input = function() {
 	this.pressedKeys = [];
 	var self = this;
 	
+	var keys = {
+		UP: 38,
+		DOWN: 40,
+		LEFT: 37,
+		RIGHT: 39
+	}
+	
 	document.addEventListener('keydown', function(event) {
 		self.pressedKeys[event.keyCode] = true;
 	});
@@ -10,8 +17,8 @@ self.Input = function() {
 		self.pressedKeys[event.keyCode] = false;
 	});
 	
-	this.isUp = function() { return self.pressedKeys[KeyEvent.DOM_VK_UP];};
-	this.isDown = function() { return self.pressedKeys[KeyEvent.DOM_VK_DOWN];};
-	this.isLeft = function() { return self.pressedKeys[KeyEvent.DOM_VK_LEFT];};
-	this.isRight = function() { return self.pressedKeys[KeyEvent.DOM_VK_RIGHT];};
+	this.isUp = function() { return self.pressedKeys[keys.UP];};
+	this.isDown = function() { return self.pressedKeys[keys.DOWN];};
+	this.isLeft = function() { return self.pressedKeys[keys.LEFT];};
+	this.isRight = function() { return self.pressedKeys[keys.RIGHT];};
 };
