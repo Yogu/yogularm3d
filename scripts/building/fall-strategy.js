ab85df026d1c8b71ffcba26a94b8953ed1be85b9
Yogu <info@yogularm.de>
2013-01-03 14:33:29 +0100

    Fall strategy calculates trace

--- scripts/building/fall-strategy.js
+++ scripts/building/fall-strategy.js
@@ -35,12 +35,10 @@
 		
 		// See how far way the target is. If it is nearer than the jump apex, the target may be as
 		// high as we can jump, otherwise use the parabola formula to calculate max height
-		var maxHeight;
-		if (distance < jumpApex[0])
-			maxHeight = source[1] + jumpApex[1];
-		else {
+		var maxHeight = source[1] + jumpApex[1];
+		if (distance > jumpApex[0]) {
 			var x = (distance - jumpApex[0]);
-			maxHeight = source[1] - x * x * parabolaFactor;
+			maxHeight -= x * x * parabolaFactor;
 		}
 		
 		console.log('    max height: ' + maxHeight)
@@ -53,8 +51,44 @@
 		console.log('    first step succeeded');
 		
 		// TODO: check if there is space
+		var trace = [];
+		var x1 = source[0];
+		var x2 = target[0];
+		var xMin = Math.min(x1, x2);
+		var xMax = Math.max(x1, x2);
+		for (var x = source[0]; x <= target[0]; x++) {
+			var z1 = source[2];
+			var z2 = target[2];
+			var zMin = Math.min(z1, z2);
+			var zMax = Math.max(z1, z2);
+			var distXSqr = (x - source[0]);
+			distXSqr *= distXSqr;
+			for (var z = zMin; z <= zMax; z++) {
+				var distZSqr = (z - source[2]);
+				distZSqr *= distZSqr;
+				var dist = Math.sqrt(distXSqr + distZSqr);
+				var yMin, yMax;
+				if (dist < jumpApex[0]) {
+					yMin = source[1];
+					yMax = source[1] + jumpApex[1] + 1 /* player height */;
+				} else {
+					dist -= jumpApex[0];
+					var parabolaY1 = source[1] - (dist - 1) * (dist - 1) * parabolaFactor;
+					var parabolaY2 = source[1] - (dist + 1) * (dist + 1) * parabolaFactor;
+					yMin = Math.floor(Math.min(parabolaY1, parabolaY2));
+					yMax = Math.ceil(Math.max(parabolaY1, parabolaY2)) + 1 /* player height */;
+				}
+				
+				// we're never lower than source _and_ target
+				yMin = Math.max(yMin, Math.min(source[1], target[1]));
+				
+				for (var y = yMin; y <= yMax; y++) {
+					trace.push([x,y,z]);
+				}
+			}
+		}
 			
-		return [];
+		return trace;
 	};};
 
 	BuildingPath.MOVE_STRATEGIES.push(FallStrategy(false));
